d48317e4fafd1e76b7212a431e6854e05fb46e27
shohruh973 <abdumannobovshohruh973@gmail.com>
2023-06-21 14:38:02 +0500

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
+import  { useReducer } from "react";
 
+const initialState = {
+  count: 0,
+};
 
-function App() {
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { count: state.count + 1 };
+    case "decrement":
+      return { count: state.count - 1 };
+    default:
+      throw new Error();
+  }
+};
+const CustomCounter = () => {
+  const [state, dispatch] = useReducer(reducer, initialState);
 
   return (
-    <>
-<h1>hello wolrd</h1>
-    </>
-  )
-}
+    <div className="max-w-screen-2xl text-center ">
+      <h1 className="text-red-500 text-center text-[25px]">Count: {state.count}</h1>
+      <button className="w-[100px] m-[20px] bg-[blue]" onClick={() => dispatch({ type: "increment" })}>Increment</button>
+      <button className="w-[100px] m-[20px]  bg-[red]" onClick={() => dispatch({ type: "decrement" })}>Decrement</button>
+    </div>
+  );
+};
 
-export default App
+export default CustomCounter;
